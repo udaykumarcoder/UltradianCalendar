04ed64bb2a702b3ecc048a357c5da6a8a203bdf1
udaykumar <methreudaykumar@gmail.com>
2025-03-07 23:23:06 +0530

    modified next.config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
